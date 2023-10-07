fd1bbbca764b6c5acdc71950db7cbc4debdc54ef
Kevin Haller <contact@kevinhaller.dev>
2023-10-07 12:10:29 +0200

    Splits up the IO code for CSV files.

--- woodcock/io/csv.py
+++ woodcock/io/csv.py
@@ -1,66 +1,9 @@
-import bz2
 import csv
-import gzip
-import lzma
-from enum import Enum
-from os import PathLike
-from typing import Iterable, Union
+from typing import Iterable
 
 from woodcock.graph.typing import Edge
-
-FilePath = Union[str, PathLike]
-
-
-class Compression(Enum):
-    GZIP = 1
-    BZIP2 = 2
-    XY = 3
-
-
-_compression_map = {
-    Compression.GZIP: lambda x: gzip.open(x, mode='rt'),
-    Compression.BZIP2: lambda x: bz2.open(x, mode='rt'),
-    Compression.XY: lambda x: lzma.open(x, mode='rt')
-}
-
-
-class _CompressionReader:
-    """A utility class for wrapping a non-compressed or compressed file."""
-
-    def __init__(self, f: FilePath, compression: Compression) -> None:
-        """Creates a new compression reader, which is a reader for a
-        non-compressed or compressed file.
-
-        Args:
-            f: the file that shall be wrapped by this reader.
-            compression: the compression type of the file. It can also be
-            `None`, which means that the file isn't compressed.
-        """
-        self._f = f
-        self._compression = compression
-
-    def __enter__(self) -> '_CompressionReader':
-        if self._compression is None:
-            self._source = open(self._f, 'r')
-        elif self._compression in _compression_map:
-            self._source = _compression_map[self._compression](self._f)
-        else:
-            raise ValueError('the compression type "%s" isn\'t supported'
-                             % self._compression.name)
-        return self
-
-    def source(self) -> Iterable[str]:
-        """Gets a TextIOWrapper for the specified file, which is an iterable
-        sequence of lines.
-
-        Returns:
-            An iterable sequence of lines.
-        """
-        return self._source
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self._source is not None:
-            self._source.close()
+from woodcock.io.typing import FilePath
+from woodcock.io.utils import Compression, CompressionReader
 
 
 def edges_from_file(f: FilePath,
@@ -86,7 +29,7 @@ def edges_from_file(f: FilePath,
         IOError: An error occurred accessing the given file.
         ValueError: The content in the given CSV file is wrongly formatted.
     """
-    with _CompressionReader(f, compression) as reader:
+    with CompressionReader(f, compression) as reader:
         reader = csv.reader(reader.source(), delimiter=delimiter)
         it = iter(reader)
         try:
---
+++ woodcock/io/utils.py
@@ -0,0 +1,59 @@
+import bz2
+import gzip
+import lzma
+from enum import Enum
+from typing import Iterable
+
+from woodcock.io.typing import FilePath
+
+
+class Compression(Enum):
+    GZIP = 1
+    BZIP2 = 2
+    XY = 3
+
+
+_compression_map = {
+    Compression.GZIP: lambda x: gzip.open(x, mode='rt'),
+    Compression.BZIP2: lambda x: bz2.open(x, mode='rt'),
+    Compression.XY: lambda x: lzma.open(x, mode='rt')
+}
+
+
+class CompressionReader:
+    """A utility class for wrapping a non-compressed or compressed file."""
+
+    def __init__(self, f: FilePath, compression: Compression) -> None:
+        """Creates a new compression reader, which is a reader for a
+        non-compressed or compressed file.
+
+        Args:
+            f: the file that shall be wrapped by this reader.
+            compression: the compression type of the file. It can also be
+            `None`, which means that the file isn't compressed.
+        """
+        self._f = f
+        self._compression = compression
+
+    def __enter__(self) -> 'CompressionReader':
+        if self._compression is None:
+            self._source = open(self._f, 'r')
+        elif self._compression in _compression_map:
+            self._source = _compression_map[self._compression](self._f)
+        else:
+            raise ValueError('the compression type "%s" isn\'t supported'
+                             % self._compression.name)
+        return self
+
+    def source(self) -> Iterable[str]:
+        """Gets a TextIOWrapper for the specified file, which is an iterable
+        sequence of lines.
+
+        Returns:
+            An iterable sequence of lines.
+        """
+        return self._source
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._source is not None:
+            self._source.close()
